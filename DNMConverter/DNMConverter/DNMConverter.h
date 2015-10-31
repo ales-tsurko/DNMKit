798273f8189bf7f212120d395ef66d37cf829c9b
jsbean <jbean46@gmail.com>
2015-10-31 14:25:31 -0400

    Added DNMConverter

---
+++ DNMConverter/DNMConverter/DNMConverter.h
@@ -0,0 +1,19 @@
+//
+//  DNMConverter.h
+//  DNMConverter
+//
+//  Created by James Bean on 10/31/15.
+//  Copyright © 2015 James Bean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DNMConverter.
+FOUNDATION_EXPORT double DNMConverterVersionNumber;
+
+//! Project version string for DNMConverter.
+FOUNDATION_EXPORT const unsigned char DNMConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DNMConverter/PublicHeader.h>
+
+
