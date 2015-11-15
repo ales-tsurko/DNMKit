08fedf3d0107f1093b26180f1d94e2ea78072035
jsbean <jbean46@gmail.com>
2015-11-15 00:22:34 -0500

    Update architecture settings for harmony between frameworks

---
+++ DNMModel/build/DNMModel.build/Debug/DNMModelTests.build/DerivedSources/DNMModelTests_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char DNMModelTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DNMModelTests  PROJECT:DNMModel-1" "\n"; const double DNMModelTestsVersionNumber __attribute__ ((used)) = (double)1.;
