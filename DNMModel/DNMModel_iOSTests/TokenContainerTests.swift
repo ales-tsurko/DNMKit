c2821bbc9dac958c0964a6000f766627b41c4be4
jsbean <jbean46@gmail.com>
2015-11-22 20:07:47 -0500

    Add tests for Token, TokenContainer

---
+++ DNMModel/DNMModel_iOSTests/TokenContainerTests.swift
@@ -0,0 +1,46 @@
+//
+//  TokenContainerTests.swift
+//  DNMModel
+//
+//  Created by James Bean on 11/22/15.
+//  Copyright © 2015 James Bean. All rights reserved.
+//
+
+import XCTest
+@testable import DNMModel
+
+class TokenContainerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInit() {
+        let tc = TokenContainer(identifier: "testContainer", openingValue: "t", startIndex: 9)
+        XCTAssert(tc.identifier == "testContainer", "id incorrect")
+        XCTAssert(tc.openingValue == "t", "opening val incorrect")
+        XCTAssert(tc.startIndex == 9, "start index incorrect")
+        XCTAssert(tc.stopIndex == 9, "stop index incorrect")
+    }
+    
+    // TODO: testDescription() 
+    
+    func testAddToken() {
+        let tc = TokenContainer(identifier: "testContainer", openingValue: "t", startIndex: 0)
+        let childToken = TokenString(identifier: "token", value: "tttt", startIndex: 2)
+        tc.addToken(childToken)
+        XCTAssert(tc.tokens.count == 1, "child token not added correctly")
+        
+        let childTokenContainer = TokenContainer(
+            identifier: "cont", openingValue: "cccc", startIndex: 4
+        )
+        tc.addToken(childTokenContainer)
+        XCTAssert(tc.tokens.count == 2, "child token container not added correctly")
+    }
+}
---
+++ DNMModel/DNMModel_iOSTests/TokenTests.swift
@@ -0,0 +1,37 @@
+//
+//  TokenTests.swift
+//  DNMModel
+//
+//  Created by James Bean on 11/22/15.
+//  Copyright © 2015 James Bean. All rights reserved.
+//
+
+import XCTest
+@testable import DNMModel
+
+class TokenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInit() {
+        let ts = TokenString(identifier: "testID", value: "testValue", startIndex: 4)
+        XCTAssert(ts.identifier == "testID", "id incorrect")
+        XCTAssert(ts.value == "testValue", "value incorrect")
+        XCTAssert(ts.startIndex == 4, "start index incorrect")
+        XCTAssert(ts.stopIndex == 12, "stop index incorrect")
+        
+        // TODO: test init for each type
+    }
+    
+    // TODO: testDescription()
+
+    // TODO: start / stop index for each type
+}
