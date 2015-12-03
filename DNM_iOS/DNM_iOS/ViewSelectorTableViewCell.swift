96bf607d0d4d1b2cadc147c92dbe42d116935eab
jsbean <jbean46@gmail.com>
2015-12-02 22:19:17 -0500

    Remove separator in ViewSelectorTableView

--- DNM_iOS/DNM_iOS/ViewSelectorTableViewCell.swift
+++ DNM_iOS/DNM_iOS/ViewSelectorTableViewCell.swift
@@ -12,7 +12,6 @@ class ViewSelectorTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
